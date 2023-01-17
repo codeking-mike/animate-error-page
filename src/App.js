5cf3712566ab1d4b7316f1b5497da9f91b1552c3
micheal idam <princemicare@gmail.com>
2023-01-17 19:08:18 +0100

    Created a 404 page and committed to Github

---
+++ src/App.js
@@ -0,0 +1,54 @@
+
+
+function App() {
+  return (
+    
+      <div className="container">
+        <div className="row">
+          
+            <div className="col-md-4">
+
+              <h1>4<i className="fa-solid fa-bounce fa-face-sad-tear text-warning"></i>4</h1>
+
+            </div>
+            <div className="col-md-6">
+              <div className="card">
+                <div className="card-body">
+                    <h2>Oops, Page not found!</h2>
+                    <p>Sorry, something must have gone wrong OR the page you looking
+                      for does'nt exists. Please comeback later or check the url for the correct address.
+                    </p>
+
+                </div>
+                <div className="card-footer">
+                  <form action="#" method="post">
+                    <p>To receive weekly updates of all our activities, events and promotion. Kindly sign up to our newsletter</p>
+                    <div className="input-group">
+                      <input type="email" className="form-control" placeholder="Enter your email" />
+                      <button className="input-group-text btn btn-warning text-white">Sign Up</button>
+                    </div>
+                    <a href="/" class="back">&laquo;&laquo;Back to home</a>
+
+                  </form>
+                </div>
+
+              </div>
+              
+            
+          </div>
+          
+        </div>
+        <div className="row">
+          <footer>
+            <div>
+              &copy; 2022 Animate. Design by clkDesignz
+            </div>
+          </footer>
+        </div>
+      </div>
+      
+  
+  );
+}
+
+export default App;
